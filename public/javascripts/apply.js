5f3037fbbefafe6fb97b5fa181eb25e49c4b37a7
wang-weifeng <980090631@qq.com>
2017-09-13 14:35:42 +0800

    <修改><apply.js  index.ejs><*>

--- public/javascripts/apply.js
+++ public/javascripts/apply.js
@@ -33,7 +33,7 @@ apply.bindEvent = function () {
         }
         // 身份证号码为15位或者18位，15位时全为数字，18位前17位为数字，最后一位是校验位，可能为数字或字符X  
         var reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
-        if (reg.test(apply_idcard) === false) {
+        if (reg.test(apply_idcard) === false || apply_idcard.length != 18) {
             //alert("身份证输入不合法");
             // layer.open({
             //     content: '身份证输入不合法-<>-'
