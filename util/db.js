2ce5dbb6756630975b29a7b39d0883295c7ed7da
wang-weifeng <980090631@qq.com>
2017-09-15 16:17:20 +0800

    <修改><数据库配置><*>

--- util/db.js
+++ util/db.js
@@ -1,7 +1,7 @@
 var dbhost = "127.0.0.1";
 var dbuser = "root";
 var dbdatabase = "kingcard";
-var dbpassword = "12456";
+var dbpassword = "";
 var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit: 10,
